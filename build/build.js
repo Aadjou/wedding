3c684b6efe0dd74312d7cb7f8ad29665fe4bb431
Madlaina Kalunder <kalunder@archilogic.com>
2018-05-22 22:12:37 +0200

    Cleanup

--- build/build.js
+++ build/build.js
@@ -1,11 +1,32 @@
-var fs = require('fs')
-// var en = require('../language/en.json')
+const fs = require('fs');
+const path = require('path');
 
-var englishJson = JSON.parse(fs.readFileSync('./src/language/EN.json', 'utf8'));
-var template = fs.readFileSync('./index.html', 'utf8')
+const languageFiles = './src/language';
+const suffix = '.json';
+const dir = path.dirname(__dirname)
 
-var englishHtml = template.replace('txt_intro', englishJson.description.what);
-fs.writeFileSync('./dist/index-en.html', englishHtml, { encoding: 'utf8' } )
+const template = fs.readFileSync('./index.html', 'utf8')
+
+
+function buildHTML(file) {
+    const filepath = path.join(dir, languageFiles, file)
+    const outputFilename = 'index-' + path.basename(file, suffix) + '.html';
+    const outputFilepath = path.join(dir, 'dist', outputFilename);
+
+    const languageFile = JSON.parse(fs.readFileSync(filepath, 'utf8'));
+
+    // Todo parse and replace
+    let parsedTemplate = template.replace('txt_intro', languageFile.description.what);
+
+    fs.writeFileSync(outputFilepath, parsedTemplate, { encoding: 'utf8' } )
+}
+
+const files = fs.readdirSync(languageFiles).filter(file => path.extname(file) == suffix);
+
+for (let file of files) {
+    buildHTML(file)
+}
+
 
 // copy css and otehr references
 
