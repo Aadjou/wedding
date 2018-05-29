0c472f696b380452b236b7aadb0dbf88f910770f
Madlaina Kalunder <kalunder@archilogic.com>
2018-05-29 22:54:04 +0200

    Updates

--- build/build.js
+++ build/build.js
@@ -3,24 +3,36 @@ const path = require('path');
 
 const languageSourcePath = './src/language';
 const suffix = '.json';
-const parentDir = path.dirname(__dirname)
+const parentDir = path.dirname(__dirname);
 
-const htmlTemplate = fs.readFileSync('./index.html', 'utf8')
+const htmlTemplate = fs.readFileSync('./index.html', 'utf8');
 
 function buildHTML(file) {
-    const filepath = path.join(parentDir, languageSourcePath, file)
+    const filepath = path.join(parentDir, languageSourcePath, file);
     const outputFilename = 'index-' + path.basename(file, suffix) + '.html';
     const outputFilepath = path.join(parentDir, 'dist', outputFilename);
 
     const languageFile = JSON.parse(fs.readFileSync(filepath, 'utf8'));
 
-    // Todo parse and replace
-    //const map = {
-    //    "intro-txt"
-    //}
-    let parsedTemplate = htmlTemplate.replace('intro-txt', languageFile.intro.txt);
+    const map = getMap(languageFile);
+    let parsedTemplate = htmlTemplate;
+    for (let key in map) {
+        parsedTemplate.replace(key, map[key]);
+    }
 
     fs.writeFileSync(outputFilepath, parsedTemplate, { encoding: 'utf8' } )
+}
+
+function getMap(file) {
+    return {
+        "main-title": file.title,
+        "main-date": file.date,
+        "main-location": file.location,
+        "intro-txt": file.intro.txt,
+        "intro-date": file.intro.date,
+        "intro-location": file.intro.location,
+        "intro-guests": file.intro.guests,
+    };
 }
 
 const files = fs.readdirSync(languageSourcePath).filter(file => path.extname(file) == suffix);
